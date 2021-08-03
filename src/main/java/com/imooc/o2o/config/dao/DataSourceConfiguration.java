14729237b8fc47de959c61a33e0f53a490aa7550
JiangTaoHou <925147954@qq.com>
2021-08-04 00:15:21 +0800

    Init springboot project

---
+++ src/main/java/com/imooc/o2o/config/dao/DataSourceConfiguration.java
@@ -0,0 +1,15 @@
+package com.imooc.o2o.config.dao;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    private String jdbcDriver;
+
+    private String jdbcUrl;
+
+    private String jdbcUsername;
+
+    private String jdbcPassword;
+}
---
+++ src/main/java/com/imooc/o2o/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.imooc.o2o.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello SpringBoot";
+    }
+}
